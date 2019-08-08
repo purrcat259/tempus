fcb13d93cd041ac9201ab2a68ea43887e87ad517
Simon Agius Muscat <simon@agius-muscat.net>
2019-08-08 22:32:07 +0200

    Wrap env vars in an interface

--- src/server/index.ts
+++ src/server/index.ts
@@ -6,9 +6,23 @@ import typeDefs from './schemas';
 import dotenv from 'dotenv';
 
 dotenv.config();
+interface Env {
+  port: number;
+  introspection: boolean;
+  playground: boolean;
+}
 
-const port: number = (process.env.PORT ? parseInt(process.env.PORT) : null) || 4000;
+const env: Env = {
+  port: (process.env.PORT ? parseInt(process.env.PORT) : null) || 4000,
+  introspection: process.env.APOLLO_INTROSPECTION ? true : false,
+  playground: process.env.APOLLO_PLAYGROUND ? true : false
+};
 
-const server: ApolloServer = new ApolloServer({ resolvers, typeDefs });
+const server: ApolloServer = new ApolloServer({
+  resolvers,
+  typeDefs,
+  introspection: env.introspection,
+  playground: env.playground
+});
 
-server.listen(port).then(({ url }) => console.log(`Server ON at: ${url}`));
+server.listen(env.port).then(({ url }) => console.log(`Server ON at: ${url}`));
