f0d7f05f1cca5460acebb45081938d11d45a93f3
Simon Agius Muscat <simon@agius-muscat.net>
2019-08-08 20:08:57 +0200

    Setup bundler

---
+++ fuse.js
@@ -0,0 +1,21 @@
+const { FuseBox } = require('fuse-box');
+
+const fuse = FuseBox.init({
+    homeDir: 'src',
+    output: 'dist/$name.js'
+})
+
+fuse.dev({ port: 4445, httpServer: false });
+
+fuse.bundle('server/bundle')
+    .watch('server/**')
+    .instructions(' > [server/index.ts]')
+    .completed(proc => proc.start())
+
+
+// fuse.bundle("client/app")
+//     .watch("client/**")
+//     .hmr()
+//     .instructions(" > client/index.ts");
+
+fuse.run();
