7068db8138fbda4245e8d238e8babac3e8fe0509
Simon Agius Muscat <simon@agius-muscat.net>
2019-08-08 20:39:49 +0200

    Added basic GQL server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+import { ApolloServer } from 'apollo-server';
+
+import resolvers from './resolvers';
+import typeDefs from './schemas';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port: number = (process.env.PORT ? parseInt(process.env.PORT) : null) || 4000;
+
+const server: ApolloServer = new ApolloServer({ resolvers, typeDefs });
+
+server.listen(port).then(({ url }) => console.log(`Server ON at: ${url}`));
